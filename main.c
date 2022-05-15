7537378901082590ea28739d4971fa3bebbfbda9
Boris Radovic <radovic.brs@gmail.com>
2022-05-15 17:43:22 +0200

    cleaned up code, removed unnecessary stuff. implemented serial code for pagerank computation with in edges

--- main.c
+++ main.c
@@ -7,8 +7,8 @@
 #include "pagerank_implementations/pagerank_custom.h"
 #include "helpers/file_helper.h"
 
-float * measure_time_custom_matrix_1(int ** edges, int * out_degrees, int nodes_count, int edges_count);
-float * measure_time_custom_matrix_2(int ** edges, int * out_degrees, int nodes_count, int edges_count);
+float * measure_time_custom_matrix_out(int ** edges, int * out_degrees, int nodes_count, int edges_count);
+float * measure_time_custom_matrix_in(int ** edges, int * in_degrees, int * out_degrees, int nodes_count, int edges_count);
 
 int main(int argc, char* argv[]) {
 
@@ -28,50 +28,57 @@ int main(int argc, char* argv[]) {
     printf("Matrix reading time: %.4f\n", end - start);
 
     // compute pagerank with multiple strategies
-    float * ref_pagerank = measure_time_custom_matrix_1(edges, out_degrees, nodes_count, edges_count);
-    float * pagerank2 = measure_time_custom_matrix_2(edges, out_degrees, nodes_count, edges_count);
+    float * ref_pagerank = measure_time_custom_matrix_out(edges, out_degrees, nodes_count, edges_count);
+    float * pagerank_in = measure_time_custom_matrix_in(edges, in_degrees, out_degrees, nodes_count, edges_count);
 
     // compare the obtained pageranks
-    compare_vectors(ref_pagerank, pagerank2, nodes_count);
+    compare_vectors(ref_pagerank, pagerank_in, nodes_count);
 
     // write the reference pagerank to file to be compared with the nx results
     write_to_file(argv[2], ref_pagerank, nodes_count);
 }
 
-float * measure_time_custom_matrix_1(int ** edges, int * out_degrees, int nodes_count, int edges_count) {
+float * measure_time_custom_matrix_out(int ** edges, int * out_degrees, int nodes_count, int edges_count) {
     double start, end;
     int ** graph;
-    printf("\nCOMPUTING PAGERANK WITH CUSTOM_MATRIX_1\n");
+    int * leaves;
+    int leaves_count;
 
+    printf("\nCOMPUTING PAGERANK WITH CUSTOM_MATRIX_OUT\n");
     start = omp_get_wtime();
-    format_graph(edges, out_degrees, &graph, nodes_count, edges_count);
+    format_graph_out(edges, out_degrees, &leaves_count, &leaves, &graph, nodes_count, edges_count);
     end = omp_get_wtime();
     printf("Matrix formatting time: %.4f\n", end - start);
-    
-    // with default init strategy
+
     start = omp_get_wtime();
-    float * pagerank = pagerank_custom_1(graph, nodes_count, 0.0000002);
+    float * pagerank = pagerank_custom_out(graph, out_degrees, leaves_count, leaves, nodes_count, 0.0000002);
     end = omp_get_wtime();
     printf("Pagerank computation time (serial): %.4f\n\n", end - start);
+    fflush(stdout);
     free(graph);
     return pagerank;
 
 }
 
-float * measure_time_custom_matrix_2(int ** edges, int * out_degrees, int nodes_count, int edges_count) {
+float * measure_time_custom_matrix_in(int ** edges, int * in_degrees, int * out_degrees, int nodes_count, int edges_count) {
+    // this implementation works ok, but is not promising as the one in which we store the outgoing edges
     double start, end;
-    int * leaves;
     int ** graph;
-    printf("\nCOMPUTING PAGERANK WITH CUSTOM_MATRIX_2\n");
+    int * leaves;
+    int leaves_count;
 
+    printf("\nCOMPUTING PAGERANK WITH CUSTOM_MATRIX_IN\n");
     start = omp_get_wtime();
-    format_graph_2(edges, out_degrees, &graph, &leaves, nodes_count, edges_count);
+    format_graph_in(edges, in_degrees, out_degrees, &leaves_count, &leaves, &graph, nodes_count, edges_count);
     end = omp_get_wtime();
     printf("Matrix formatting time: %.4f\n", end - start);
-
+    
     start = omp_get_wtime();
-    float * pagerank = pagerank_custom_2(graph, out_degrees, leaves, nodes_count, 0.0000002);
+    float * pagerank = pagerank_custom_in(graph, in_degrees, out_degrees, leaves_count, leaves, nodes_count, 0.0000002);
     end = omp_get_wtime();
     printf("Pagerank computation time (serial): %.4f\n\n", end - start);
+    fflush(stdout);
+    free(graph);
     return pagerank;
+
 }
